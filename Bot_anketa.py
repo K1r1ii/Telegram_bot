bcad8f762b5ebded27bc79d7f3c4e800f80fd205
K1r1ii <kirill.salnikov.2004@mail.ru>
2022-10-03 22:03:20 +0300

    Added message processing

--- Bot_anketa.py
+++ Bot_anketa.py
@@ -8,9 +8,25 @@ logging.basicConfig(level=logging.INFO)
 bot = Bot(token=API_TOKEN)
 dp = Dispatcher(bot)
 
-@dp.message_handler()
+#
+@dp.message_handler(commands=['start', 'help'])
 async def send_welcome(message: types.Message):
-    await message.reply(message.text)
+    if message.text == '/start':
+        await message.reply('Hi, i am Bot in aiogram! Write me)')
+    elif message.text == '/help':
+        await message.reply('Пиши "Привет" или "Что ты умеешь?"')
+
+@dp.message_handler()
+async def answer(message: types.Message):
+    if message.text == "Привет":
+        await message.reply('Hi')
+    elif message.text == 'Что ты умеешь?':
+        await message.reply('Пока что умею только повторять за тобой, но скоро я стану круче')
+    else:
+        await message.reply(message.text)
+
+
+
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
